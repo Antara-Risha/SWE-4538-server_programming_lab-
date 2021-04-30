46f22626e9f56c0e66ad1569f318b4ce3f9a6698
Antara-Risha <antararisha@iut-dhaka.edu>
2021-04-30 12:36:27 +0600

    require module proccess

--- 180042146/lab 1/globalVariables.js
+++ 180042146/lab 1/globalVariables.js
@@ -3,3 +3,10 @@ const currentDirectory = __dirname;
 console.log(currentDirectory);
 //__filename =file name
 console.log(__filename);
+//require= function to use module
+const fs = require('fs');
+console.log(fs);
+//module =info about current module
+console.log(module);
+//process = info about current environment
+console.log(process);
