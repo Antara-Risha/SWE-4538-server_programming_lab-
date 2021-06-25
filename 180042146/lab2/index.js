a0d768fc826a684e2fb0b46af5c1689e8df3f0d0
Antara-Risha <antararisha@iut-dhaka.edu>
2021-06-26 02:03:41 +0600

    npm start successful

--- 180042146/lab2/index.js
+++ 180042146/lab2/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
 
+//import Routes
+const authRoute = require('./routes/auth');
+
+//Route Middlewires
+app.use('/api/user',authRoute);
+
+
+
+
 app.listen(3000, () => console.log('Server up and running'));
 
