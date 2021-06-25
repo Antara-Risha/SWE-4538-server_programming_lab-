39dcd42d290750a7112d7275fdf9548c8bdea707
Antara-Risha <antararisha@iut-dhaka.edu>
2021-06-26 03:08:29 +0600

    mongodb databse connected

--- 180042146/lab2/index.js
+++ 180042146/lab2/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+//conncet to db
+mongoose.connect('mongodb+srv://raisa:dhupa1468@cluster0.vfn3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useUnifiedTopology: true } ,
+() => console.log('connected to DB'));
 //import Routes
 const authRoute = require('./routes/auth');
 
