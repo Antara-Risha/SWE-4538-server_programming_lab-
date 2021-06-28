8cba6b70b302c8e77cb7f1143392f05b53c8f4e7
Antara-Risha <antararisha@iut-dhaka.edu>
2021-06-28 22:57:21 +0600

    complete

--- 180042146/lab2/routes/auth.js
+++ 180042146/lab2/routes/auth.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userSchema = require('../model/User');
 const bcrypt = require('bcryptjs');
-const bodyParser = require("body-parser");\
+const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
 const {registerValidation,loginValidation} = require('../validation');
+const { exists } = require('../model/User');
 
 //validation
 
@@ -40,8 +42,8 @@ router.post('/register', async (req, res) => {
     
    try {
     const savedUser = await 
-    res.send(savedUser);
-    console.log('rasia');
+    res.send({user: user_id});
+    //console.log('rasia');
  } catch(err){
     res.status(400).send(err);
 
@@ -49,6 +51,30 @@ router.post('/register', async (req, res) => {
       
       
    
+
+});
+
+
+
+
+//login
+router.post('/login', async(req ,res)=> {
+    const {error} =loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const user = await user.findOne({email: req.body.email});
+
+  if ( !user) return res.status(400).send('Email dosent exists') ;
+
+
+  // password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if(!validPass) return res.status(400).send('invalid password');
+
+  // create and assign a token 
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth_token', token).send(token);
+
+  //res.send('logged in');
 
 });
 
