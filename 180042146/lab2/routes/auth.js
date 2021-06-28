6008e1e1fde6fb24da701955a75a8ed59e37e0c1
Antara-Risha <antararisha@iut-dhaka.edu>
2021-06-28 21:48:59 +0600

    views added and password hashed validation added

--- 180042146/lab2/routes/auth.js
+++ 180042146/lab2/routes/auth.js
@@ -1,17 +1,37 @@
 const router = require('express').Router();
 const userSchema = require('../model/User');
-const bodyParser = require("body-parser");
+const bcrypt = require('bcryptjs');
+const bodyParser = require("body-parser");\
+const {registerValidation,loginValidation} = require('../validation');
+
+//validation
+
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
+ 
 
 
 router.post('/register', async (req, res) => {
+    // valodate the data before we a user 
+  const {error} =registerValidation(req.body);
+
+  if(error) return res.status(400).send(error.details[0].message);
+
+
+  //check if the user is already in database
+  const emailExist = await user.findOne({email: req.body.email});
+
+  if ( emailExist) return res.status(400).send('Email alrady exists');
+// hash password
+   const salt = await bcrypt.genSalt(10);
+   const hashedPassword = await bcrypt.hash(req.body.password,salt);
+
     // const {name,email,password} = req.body;
    const user = {
        name: req.body.name,
        email: req.body.email,
-       password: req.body.password
+       password: hashedPassword
    };
 
     //   const savedUser = await new userSchema(user).save();
