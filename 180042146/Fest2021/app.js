0e920aae950543ebb983564e80e08c91328b27c7
Antara-Risha <antararisha@iut-dhaka.edu>
2021-07-01 03:13:55 +0600

    db connected and data store successful

--- 180042146/Fest2021/app.js
+++ 180042146/Fest2021/app.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
+const mongoose = require('mongoose');
+//connect to DB
+mongoose.connect(process.env.MongoURI,{useNewUrlParser: true,
+    useUnifiedTopology: true,})
+    .then(() => {
+        console.log("Connected to Database!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
 //static resource 
 app.use(express.static("public"));
@@ -25,6 +36,7 @@ app.use(express.urlencoded({ extended:false}));
 //Routes
 const indexRoutes = require("./routes/index.routes");
 const userRoutes = require("./routes/users.routes");
+const { connect } = require('./routes/index.routes');
 app.use(indexRoutes);
 app.use("/users",userRoutes);
 
