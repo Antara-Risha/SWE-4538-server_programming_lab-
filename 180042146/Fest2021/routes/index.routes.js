0d9c93b91dd4298b2483bd8b1fccffd4a14d393d
Antara-Risha <antararisha@iut-dhaka.edu>
2021-06-30 23:17:30 +0600

    users route complete

--- 180042146/Fest2021/routes/index.routes.js
+++ 180042146/Fest2021/routes/index.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get("/",(req,res)=>{
     //res.render("users/login.ejs");
-    res.render("users/register.ejs");
+    res.render("welcome.ejs");
+    //res.render("users/register.ejs");
     //res.render("dashboard.ejs");
 });
  module.exports = router;
