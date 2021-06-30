89c0f629451f6f1c1c60a9f35af1ab032dbfe15a
Antara-Risha <antararisha@iut-dhaka.edu>
2021-07-01 02:11:31 +0600

    error checking compete nxt database

--- 180042146/Fest2021/app.js
+++ 180042146/Fest2021/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //static resource 
 app.use(express.static("public"));
 
 //view Engine
 app.set("view engine", "ejs");
+
+//Session and Flash
+app.use(
+    session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+  app.use(flash());
+
+//Body purser
+app.use(express.urlencoded({ extended:false}));
 
 //Routes
 const indexRoutes = require("./routes/index.routes");
--- 180042146/Fest2021/controllers/users.controller.js
+++ 180042146/Fest2021/controllers/users.controller.js
@@ -1,13 +1,42 @@
 const getLogin = (req,res) =>{
     res.render("users/login.ejs");
 };
- const postLogin = ()=>{};
+ const postLogin = (req,res)=>{
+     const {email,password} =req.body;
+     console.log(email);
+     console.log(password);
+ };
   
  const getRegister = (req,res) =>{
-     res.render("users/register.ejs");
+     res.render("users/register.ejs",{errors:req.flash('errors')});
  };
 
- const postRegister = () =>{};
+ const postRegister = (req, res ) =>{
+     const {name, email, password, confirm_password } = req.body;
+     //Data Validation
+  const errors = [];
+  if (!name || !email || !password || !confirm_password) {
+    errors.push("All fields are required!");
+  }
+  if (password.length < 6) {
+    errors.push("Password must be at least 6 characters!");
+  }
+  if (password !== confirm_password) {
+    errors.push("Passwords do not match!");
+  }
+
+  if (errors.length > 0) {
+    req.flash("errors", errors);
+    res.redirect("/users/register");
+  } else {
+    //Create New User
+    res.redirect("/users/login");
+  }
+    //  console.log(name);
+    //  console.log(email);
+    //  console.log(password);
+    //  console.log(confirm_password);
+ };
 
 
 
