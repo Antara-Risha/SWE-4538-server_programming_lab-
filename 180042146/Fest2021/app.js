bfa7f17e0e1afdd4d42d77f8203af02ae1899d34
Antara-Risha <antararisha@iut-dhaka.edu>
2021-06-30 22:41:07 +0600

    tamplate added and process complete

--- 180042146/Fest2021/app.js
+++ 180042146/Fest2021/app.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
 
+//static resource 
+app.use(express.static("public"));
+
+//view Engine
+app.set("view engine", "ejs");
+
+//Routes
+const indexRoutes = require("./routes/index.routes");
+app.use(indexRoutes);
+
+
 module.exports = app;
---
+++ 180042146/Fest2021/routes/index.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    //res.render("users/login.ejs");
+    res.render("users/register.ejs");
+    //res.render("dashboard.ejs");
+});
+ module.exports = router;
