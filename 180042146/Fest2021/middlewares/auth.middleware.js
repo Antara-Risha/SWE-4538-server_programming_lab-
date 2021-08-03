a4bbfa55dfe26fd11678383a1736587231d09158
Antara-Risha <antararisha@iut-dhaka.edu>
2021-08-04 03:20:12 +0600

    restrictedKonoPageGeleAuthenticatUserArInfoMilabe

--- 180042146/Fest2021/middlewares/auth.middleware.js
+++ 180042146/Fest2021/middlewares/auth.middleware.js
@@ -8,4 +8,11 @@ const ensureAuthenticated=(req,res,next)=>
         res.redirect("/users/login");
     }
 };
+const  addUserData = (req, res, next) =>
+{
+    res.locals.req =req;
+    res.locals.res = res;
+    next();
+
+};
 module.exports = ensureAuthenticated;
