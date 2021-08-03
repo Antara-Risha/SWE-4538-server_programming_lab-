43d6f15d52b2c0ca74bb40368491c2781d7be3eb
Antara-Risha <antararisha@iut-dhaka.edu>
2021-08-04 03:31:51 +0600

    both middleware func call Math suru

--- 180042146/Fest2021/routes/index.routes.js
+++ 180042146/Fest2021/routes/index.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ensureAuthenticated = require("./../middlewares/auth.middleware");
+const {ensureAuthenticated, addUserData} = require("./../middlewares/auth.middleware");
 
 router.get("/",(req,res)=>{
     //res.render("users/login.ejs");
@@ -9,7 +9,7 @@ router.get("/",(req,res)=>{
     //res.render("dashboard.ejs");
 });
 
-router.get("/dashboard", ensureAuthenticated,(req, res)=>{
+router.get("/dashboard", ensureAuthenticated,addUserData,(req, res)=>{
 res.render("dashboard.ejs",{ user: req.user});
 });
  module.exports = router;
