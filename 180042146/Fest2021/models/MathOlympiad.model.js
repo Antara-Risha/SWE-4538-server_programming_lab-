9347bc41c0f81cd6782599ae0b5fea815f7065c5
Antara-Risha <antararisha@iut-dhaka.edu>
2021-08-04 05:51:24 +0600

    Cannot GET /MathOlympiad/register

---
+++ 180042146/Fest2021/models/MathOlympiad.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const MOSchema = new.mongoose.Schema({
+    name:{
+        type : String,
+        require: true,
+    },
+    category:{
+        type : String,
+        require: true,
+    },
+    contact:{
+        type : String,
+        require: true,
+    },
+    email:{
+        type : String,
+        require: false,
+    },
+    institution:{
+        type : String,
+        require: true,
+    },
+    total:{
+        type : Number,
+        require: true,
+    },
+    paid:{
+        type : Number,
+        require: true,
+    },
+    selected:{
+        type : Boolean,
+        require: true,
+    },
+    tshirt:{
+        type : String,
+        require: true,
+    },
+    date:{
+        type : date,
+        default: Date.now,
+    },
+
+});
+
+const MathOlympiad = mongoose.model("MathOlympiad", MOSchema);
+module.exports = MathOlympiad;
