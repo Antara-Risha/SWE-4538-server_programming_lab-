c2a62e4f008ca8e5102689e674325e87bc8e310e
Antara-Risha <antararisha@iut-dhaka.edu>
2021-08-06 00:58:43 +0600

    error check and participant registration

--- 180042146/Fest2021/controllers/mathOlympiad.controller.js
+++ 180042146/Fest2021/controllers/mathOlympiad.controller.js
@@ -11,7 +11,54 @@ const postMO = (req,res) => {
     console.log(email);
     console.log(institution);
     console.log(tshirt);
-    res.render("math-olympiad/register.ejs");
+
+    let registrationFee = 0;
+  if (category == "School") {
+    registrationFee = 250;
+  } else if (category == "College") {
+    registrationFee = 400;
+  } else {
+    registrationFee = 500;
+  }
+
+  const total = registrationFee;
+  const paid = 0;
+  const selected = false;
+
+  let error = "";
+
+  MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
+    if (participant) {
+      error = "Participant with this name and contact number already exists!";
+      req.flash("error", error);
+      res.redirect("/MathOlympiad/register");
+    } else {
+      const participant = new MathOlympiad({
+        name,
+        category,
+        contact,
+        email,
+        institution,
+        paid,
+        total,
+        selected,
+        tshirt,
+      });
+      participant
+        .save()
+        .then(() => {
+          error = "Participant has been registered successfully!";
+          req.flash("error", error);
+          res.redirect("/MathOlympiad/register");
+        })
+        .catch(() => {
+          error = "An unexpected error occured while registering participant";
+          req.flash("error", error);
+          res.redirect("/MathOlympiad/register");
+        });
+    }
+  });
+   
 };
 const getMOList = (req,res) => {
     res.render("math-olympiad/list.ejs");
